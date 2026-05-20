b530eb92ae15cf8ecd1bbbb13d4ac2c1ebbe6ce9
Tushar Shihab <tushar.shihab13@gmail.com>
2026-05-20 23:56:30 +0600

    feat(config): update backend template with custom setup

--- config/settings.py
+++ config/settings.py
@@ -77,17 +77,20 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.global_context',
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
