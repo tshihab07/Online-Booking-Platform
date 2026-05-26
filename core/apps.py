482671711efdf9b0ee984c993883b798aa01caf9
Tushar Shihab <tushar.shihab13@gmail.com>
2026-05-26 17:07:33 +0600

    fix: disconnect create_permissions signal to prevent post-migrate crash on MongoDB

--- core/apps.py
+++ core/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+        post_migrate.disconnect(
+            dispatch_uid='django.contrib.auth.management.create_permissions'
+        )
