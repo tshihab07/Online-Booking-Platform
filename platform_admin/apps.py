b560a4ec8fa3547308d8bfeb1fc6680b324de0b6
Tushar Shihab <tushar.shihab13@gmail.com>
2026-06-22 23:47:20 +0600

    chore(platform_admin): set default auto field to MongoDB ObjectIdAutoField

--- platform_admin/apps.py
+++ platform_admin/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class PlatformAdminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField'
     name = 'platform_admin'
     verbose_name = 'Platform Administration'
